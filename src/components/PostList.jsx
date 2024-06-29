1fddf7d307113567fe79540b6c9ca9e496b1f582
JJ807 <jakubstrugaa@gmail.com>
2024-06-29 18:29:52 +0200

    1st excercise solution

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,8 +1,9 @@
 import Post from './Post';
+import classes from './PostList.module.css';
 
 function PostList() {
     return(
-    <ul>
+    <ul className={classes.posts}>
         <Post body="mean tweet" author="everyone" />
         <Post body="happy tweet" author="noone" />
     </ul>
