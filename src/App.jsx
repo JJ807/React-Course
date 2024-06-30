1c6c2584c113e8eb28b6463d4b58a010d2e84ccc
JJ807 <jakubstrugaa@gmail.com>
2024-06-30 14:20:28 +0200

    state and lifting it up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import PostList from './components/PostList';
+import MainHeader from './components/MainHeader';
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
   return (
     <main>
-      <PostList/>
+      <MainHeader onCreatePost={showModalHandler} />
+      <PostList isPosting={modalIsVisible} onStopPosting={hideModalHandler} />
     </main>)
 }
 
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,32 @@
-import Post from './Post';
-import classes from './PostList.module.css';
-import NewPost from './NewPost';
+import Post from "./Post";
+import classes from "./PostList.module.css";
+import NewPost from "./NewPost";
+import { useState } from "react";
+import Modal from "./Modal";
 
-function PostList() {
+function PostList({ isPosting, onStopPosting }) {
+    const [enteredBody, setEnteredBody] = useState("");
+    const [enteredAuthor, setEnteredAuthor] = useState("");
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
     return (
         <>
-            <NewPost />
+            {isPosting && (<Modal onClose={onStopPosting}>
+                <NewPost
+                    onBodyChange={bodyChangeHandler}
+                    onAuthorChange={authorChangeHandler}
+                    onCancel={onStopPosting}
+                />
+            </Modal>)}
             <ul className={classes.posts}>
-                <Post body="mean tweet" author="everyone" />
-                <Post body="happy tweet" author="noone" />
+                <Post author={enteredAuthor} body={enteredBody} />
+                <Post author="noone" body="happy tweet" />
             </ul>
         </>
     );
