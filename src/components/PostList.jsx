41ea42297cebfb630684ee8c77b1095179b384b2
JJ807 <jakubstrugaa@gmail.com>
2024-07-15 17:11:47 +0200

    using Links, actions, routing and redirect

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,22 +1,9 @@
 import Post from "./Post";
 import classes from "./PostList.module.css";
-import { useState, useEffect } from "react";
+import { useLoaderData } from 'react-router-dom';
 
 function PostList() {
-    const [posts, setPosts] = useState([]);
-    const [isFetching, setIsFetching] = useState(false);
-
-    useEffect(() => {
-        async function fetchPosts() {
-            setIsFetching(true)
-            const response = await fetch('http://localhost:8080/posts')
-            const resData = await response.json();
-            setPosts(resData.posts);
-            setIsFetching(false);
-        }
-
-        fetchPosts();
-    }, []);
+    const posts = useLoaderData();
 
     function addPostHandler(postData) {
         fetch('http://localhost:8080/posts', {
@@ -31,15 +18,13 @@ function PostList() {
 
     return (
         <>
-            {!isFetching && posts.length > 0 && (
+            {posts.length > 0 && (
                 <ul className={classes.posts}>
                     {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
                 </ul>)}
-            {!isFetching && posts.length === 0 && (<div style={{ textAlign: 'center', color: 'white' }}>
+            {posts.length === 0 && (<div style={{ textAlign: 'center', color: 'white' }}>
                 <h2>There are not posts yet</h2>
                 <p>start adding some</p>
-            </div>)}
-            {isFetching && (<div style={{ textAlign: 'center', color: 'white' }}><p>Loading posts</p>
             </div>)}
         </>
     );
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,47 +1,40 @@
 import classes from './NewPost.module.css';
-import { useState } from "react";
 import Modal from '../components/Modal';
+import { Link, Form } from 'react-router-dom';
 
-function NewPost({ onCancel, onAddPost }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
 
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  function submitHandler(event) {
-    event.preventDefault();
-    const postData = {
-      body: enteredBody,
-      author: enteredAuthor
-    };
-    console.log(postData);
-    onAddPost(postData);
-    onCancel();
-  }
-
+function NewPost() {
   return (
     <Modal>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <Form method='post' className={classes.form}>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
+          <textarea id="body" name="body" required rows={3} />
         </p>
         <p>
           <label htmlFor="name">Your name</label>
-          <input type="text" id="name" required onChange={authorChangeHandler} />
+          <input type="text" name="author" id="name" required />
         </p>
         <p className={classes.actions}>
-          <button type="button" onClick={onCancel}>Cancel</button>
+          <Link to=".." type="button">
+            Cancel
+          </Link>
           <button>Submit</button>
         </p>
-      </form>
+      </Form>
     </Modal>
   );
 }
 
 export default NewPost;
+
+export function action({ request }) {
+  request.formData();
+  fetch('http://localhost:8080/posts', {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
